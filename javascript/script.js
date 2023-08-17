46a4424d9bf794e6d59fa9477a25d171efc5995d
catalogo feel move <124948797+eservente87@users.noreply.github.com>
2023-08-16 22:49:48 -0300

    se agrega menu de navegación a las páginas

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,7 @@
-document.querySelector(".bars__menu").addEventListener("click", animateBars);
+let menuHamburguesa = document.querySelector(".nav__logo")
 
-var line1__bars = document.querySelector(".line1__bars--menu");
-var line2__bars = document.querySelector(".line2__bars--menu");
-var line3__bars = document.querySelector(".line3__bars--menu");
-var container__menu = document.querySelector(".main__hero")
+menuHamburguesa.addEventListener("click", ()=>{
+    let menu = document.querySelector(".main__links");
 
-function animateBars(){
-    line1__bars.classList.toggle("activeline1__bars--menu");
-    line2__bars.classList.toggle("activeline2__bars--menu");
-    line3__bars.classList.toggle("activeline3__bars--menu");
-
-    var container__menu = document.querySelector(".menu__active")
-}
+    menu.classList.toggle("list--show")
+})
